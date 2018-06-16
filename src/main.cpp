608acf0938a046f19492363a5941775c75aff285
mabergmann <32148891+mabergmann@users.noreply.github.com>
2018-06-16 18:31:13 -0300

    Added function to load obj file

--- src/main.cpp
+++ src/main.cpp
@@ -40,6 +40,9 @@
 #include "matrices.h"
 
 #include <iostream>
+#include <vector>
+
+using namespace std;
 
 // Declaração de várias funções utilizadas em main().  Essas estão definidas
 // logo após a definição de main() neste arquivo.
@@ -124,6 +127,39 @@ glm::vec4 camera_up_vector; // Vetor "up" fixado para apontar para o "céu" (eit
 
 // Variável que controla se o texto informativo será mostrado na tela.
 bool g_ShowInfoText = true;
+
+void load_obj(const char* filename, vector<glm::vec4> &vertices, vector<glm::vec3> &normals, vector<GLushort> &elements) {
+  ifstream in(filename, ios::in);
+  if (!in) { cerr << "Não pode abrir o arquivo: " << filename << endl; exit(1); }
+
+  string line;
+  while (getline(in, line)) {
+    if (line.substr(0,2) == "v ") {
+      istringstream s(line.substr(2));
+      glm::vec4 v; s >> v.x; s >> v.y; s >> v.z; v.w = 1.0f;
+      vertices.push_back(v);
+    }  else if (line.substr(0,2) == "f ") {
+      istringstream s(line.substr(2));
+      GLushort a,b,c;
+      s >> a; s >> b; s >> c;
+      a--; b--; c--;
+      elements.push_back(a); elements.push_back(b); elements.push_back(c);
+    }
+    else if (line[0] == '#') { /* ignorando esta linha */ }
+    else { /* ignoring this line */ }
+  }
+
+  normals.resize(vertices.size(), glm::vec3(0.0, 0.0, 0.0));
+  for (int i = 0; i < elements.size(); i+=3) {
+    GLushort ia = elements[i];
+    GLushort ib = elements[i+1];
+    GLushort ic = elements[i+2];
+    glm::vec3 normal = glm::normalize(glm::cross(
+      glm::vec3(vertices[ib]) - glm::vec3(vertices[ia]),
+      glm::vec3(vertices[ic]) - glm::vec3(vertices[ia])));
+    normals[ia] = normals[ib] = normals[ic] = normal;
+  }
+}
 
 int main()
 {
