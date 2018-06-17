7cf4e9b6297a47c0bcf4ff993fdbd0dfd33be686
mabergmann <32148891+mabergmann@users.noreply.github.com>
2018-06-17 11:13:08 -0300

    Solved merge

--- include/Carro.h
+++ include/Carro.h
@@ -4,32 +4,15 @@
 
 class Carro
 {
-<<<<<<< HEAD
     private:
          glm::mat4 matrix = Matrix_Identity();
          float speed = 0.5f;
          double last_time;
-=======
-    public:
-         <glm::mat4> matriz = Matrix_Identity();
-         float velocidade = 0.5f;
-         double ultimotempo = glfwGetTime();
->>>>>>> 0ecabcf0fcf0861acd4d7e9182d6aac603c45dc8
 
     public:
         Carro();
         virtual ~Carro();
-<<<<<<< HEAD
         moveCarro(double tempoagora);
-=======
-
-    public:
-        moveCarro(double tempoagora){
-            matrix = matriz *
-            (tempoagora-(*ultimotempo)*Matrix_Translate(1.0f,0.0f,0.0f));
-            ultimotempo = glfwGetTime();
-        }
->>>>>>> 0ecabcf0fcf0861acd4d7e9182d6aac603c45dc8
 
 };
 
