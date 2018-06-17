0ecabcf0fcf0861acd4d7e9182d6aac603c45dc8
HenriqueGoetz <henrique.goetz@gmail.com>
2018-06-17 11:07:44 -0300

    correcao do atributo ultimotempo

--- include/Carro.h
+++ include/Carro.h
@@ -7,15 +7,17 @@ class Carro
     public:
          <glm::mat4> matriz = Matrix_Identity();
          float velocidade = 0.5f;
+         double ultimotempo = glfwGetTime();
 
     public:
         Carro();
         virtual ~Carro();
 
     public:
-        moveCarro(double tempoagora, double *ultimo){
+        moveCarro(double tempoagora){
             matrix = matriz *
             (tempoagora-(*ultimotempo)*Matrix_Translate(1.0f,0.0f,0.0f));
+            ultimotempo = glfwGetTime();
         }
 
 };
